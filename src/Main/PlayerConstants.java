1f731532244bc31bc482dd226f9eb0567696b719
sanjuGoudru <sanjugr7022@gmail.com>
2019-09-12 17:09:19 +0530

    Update PlayerConstants.java

--- src/Main/PlayerConstants.java
+++ src/Main/PlayerConstants.java
@@ -20,4 +20,17 @@ public final class PlayerConstants {
 		static final int LEFT_PACER=3;
 		static final int LEFT_SPINNER=4;
 	}
+	public static class Country{
+		static final int ANYTHING=0;
+		static final int INDIA=1;
+		static final int AUSTRALIA=2;
+		static final int ENGLAND=3;
+		static final int SOUTH_AFRICA=4;
+		static final int NEW_ZEALAND=5;
+		static final int PAKISTAN=6;
+		static final int SRI_LANKA=7;
+		static final int WEST_INDIES=8;
+		static final int BANGLADESH=9;
+		static final int AFGHANISTAN=10;
+	}
 }
