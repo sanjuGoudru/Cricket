e5756875003f42c6fef44682b59b00ecfd0c0bbe
sanjuGoudru <sanjugr7022@gmail.com>
2019-09-12 16:41:23 +0530

    Create PlayerConstants.java

---
+++ src/Main/PlayerConstants.java
@@ -0,0 +1,23 @@
+package Main;
+
+public final class PlayerConstants {
+	public static class Role{
+		static final int ANYTHING = 0;
+		static final int BATSMAN = 1;
+		static final int BOWLER = 2;
+		static final int BATTING_ALLROUNDER = 3;
+		static final int BOWLING_ALLROUNDER= 4;
+	}
+	public static class BattingStyle{
+		static final int ANYTHING = 0;
+		static final int RIGHT_HANDED = 1;
+		static final int LEFT_HANDED = 2;
+	}
+	public static class BowlingStyle{
+		static final int ANYTHING = 0;
+		static final int RIGHT_PACER = 1;
+		static final int RIGHT_SPINNER=2;
+		static final int LEFT_PACER=3;
+		static final int LEFT_SPINNER=4;
+	}
+}
