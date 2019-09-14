df19a9ef4fcaf6bb411c23b243931a0efb527c3c
sanjuGoudru <sanjugr7022@gmail.com>
2019-09-14 17:08:04 +0530

    Implemented findPlayer method and modified PlayerConstants class

--- src/Main/PlayerConstants.java
+++ src/Main/PlayerConstants.java
@@ -1,36 +1,44 @@
 package Main;
 
 public final class PlayerConstants {
+	public static final int MAX_ROLES = 4;
+	public static final int MAX_BATTING_STYLE=2;
+	public static final int MAX_BOWLING_STYLE = 4;
+	public static final int MAX_COUNTRIES = 10;
+	public static final int MIN_ROLES = 0;
+	public static final int MIN_BATTING_STYLE=0;
+	public static final int MIN_BOWLING_STYLE = 0;
+	public static final int MIN_COUNTRIES = 0;
 	public static class Role{
-		static final int ANYTHING = 0;
-		static final int BATSMAN = 1;
-		static final int BOWLER = 2;
-		static final int BATTING_ALLROUNDER = 3;
-		static final int BOWLING_ALLROUNDER= 4;
+		public static final int ANYTHING = 0;
+		public static final int BATSMAN = 1;
+		public static final int BOWLER = 2;
+		public static final int BATTING_ALLROUNDER = 3;
+		public static final int BOWLING_ALLROUNDER= 4;
 	}
 	public static class BattingStyle{
-		static final int ANYTHING = 0;
-		static final int RIGHT_HANDED = 1;
-		static final int LEFT_HANDED = 2;
+		public static final int ANYTHING = 0;
+		public static final int RIGHT_HANDED = 1;
+		public static final int LEFT_HANDED = 2;
 	}
 	public static class BowlingStyle{
-		static final int ANYTHING = 0;
-		static final int RIGHT_PACER = 1;
-		static final int RIGHT_SPINNER=2;
-		static final int LEFT_PACER=3;
-		static final int LEFT_SPINNER=4;
+		public static final int ANYTHING = 0;
+		public static final int RIGHT_PACER = 1;
+		public static final int RIGHT_SPINNER=2;
+		public static final int LEFT_PACER=3;
+		public static final int LEFT_SPINNER=4;
 	}
 	public static class Country{
-		static final int ANYTHING=0;
-		static final int INDIA=1;
-		static final int AUSTRALIA=2;
-		static final int ENGLAND=3;
-		static final int SOUTH_AFRICA=4;
-		static final int NEW_ZEALAND=5;
-		static final int PAKISTAN=6;
-		static final int SRI_LANKA=7;
-		static final int WEST_INDIES=8;
-		static final int BANGLADESH=9;
-		static final int AFGHANISTAN=10;
+		public static final int ANYTHING=0;
+		public static final int INDIA=1;
+		public static final int AUSTRALIA=2;
+		public static final int ENGLAND=3;
+		public static final int SOUTH_AFRICA=4;
+		public static final int NEW_ZEALAND=5;
+		public static final int PAKISTAN=6;
+		public static final int SRI_LANKA=7;
+		public static final int WEST_INDIES=8;
+		public static final int BANGLADESH=9;
+		public static final int AFGHANISTAN=10;
 	}
 }
