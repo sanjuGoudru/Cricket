19a6edc3ba0d145d5ffb4e54dbf7c0fa29d15d16
sanjuGoudru <sanjugr7022@gmail.com>
2019-09-27 16:37:27 +0530

    function getAllTeams() created

--- src/DAO/LeagueDAO.java
+++ src/DAO/LeagueDAO.java
@@ -2,7 +2,12 @@ package DAO;
 
 import java.sql.Connection;
 import java.sql.DriverManager;
+import java.sql.ResultSet;
 import java.sql.SQLException;
+import java.sql.Statement;
+import java.util.ArrayList;
+
+import Main.*;
 
 public class LeagueDAO {
 	Connection con = null;
@@ -26,5 +31,18 @@ public class LeagueDAO {
 			System.out.println("Error in closing the connection");
 			e.printStackTrace();
 		}
+	}
+
+	public ArrayList<Team> getAllTeams(int lid) throws SQLException {
+		ArrayList<Team> teams = new ArrayList<Team>();
+		String query = "select * from team where lid = " + lid;
+		Statement st = null;
+		ResultSet rs = null;
+
+		st = con.createStatement();
+		rs = st.executeQuery(query);
+		while (rs.next())
+			teams.add(new Team(rs.getInt(1), rs.getInt(2), rs.getString(3), rs.getString(4)));
+		return teams;
 	}
 }
