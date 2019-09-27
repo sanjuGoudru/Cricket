5a97e05524bff29fff77feebc992faffa7a29675
18satwik <41579386+18satwik@users.noreply.github.com>
2019-09-27 16:11:18 +0530

    Update LeagueDAO.java

--- src/DAO/LeagueDAO.java
+++ src/DAO/LeagueDAO.java
@@ -1,5 +1,30 @@
 package DAO;
 
-public class LeagueDAO {
+import java.sql.Connection;
+import java.sql.DriverManager;
+import java.sql.SQLException;
 
+public class LeagueDAO {
+	Connection con = null;
+	static final String url = "jdbc:mysql://localhost:3306/cricket";
+	static final String username = "root";
+	static final String password = "root";
+
+	public void connect() {
+		try {
+			con = DriverManager.getConnection(url, username, password);
+		} catch (SQLException e) {
+			System.out.println("Error in setting connection");
+			e.printStackTrace();
+		}
+	}
+
+	public void close() {
+		try {
+			con.close();
+		} catch (SQLException e) {
+			System.out.println("Error in closing the connection");
+			e.printStackTrace();
+		}
+	}
 }
